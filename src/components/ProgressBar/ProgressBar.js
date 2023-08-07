85a73c561f4c4297acc4b901e60f04b81ea887da
Polina Kasatkina <p.s.kasatkina@gmail.com>
2023-08-07 15:23:56 +0400

    Workshop: Mini component library

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,59 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const SIZES = {
+  small: {
+    "--heigth" : "8px",
+    "--borderRadius": 4 + "px",
+    "--padding": "0"
+  },
+  medium: {
+    "--heigth" : "12px",
+    "--borderRadius": 4 + "px",
+    "--padding": "0"
+  },
+  large: {
+    "--heigth" : "24px",
+    "--borderRadius": 8 + "px",
+    "--padding": "4px"
+  }
 };
+
+const ProgressBar = ({ value, size }) => {
+
+  const styles = SIZES[size];
+
+  return <ProgressWrapper style={styles} role="progressbar" aria-valuemin={0} aria-valuemax={100} aria-valuenow={value}>
+    <VisuallyHidden>{value}%</VisuallyHidden>
+    <BarWrapper>
+      <Bar style={{ "--width" : value + '%' }} />
+    </BarWrapper>
+  </ProgressWrapper>
+  ;
+};
+
+const ProgressWrapper = styled.div`
+    background-color: ${COLORS.transparentGray15};
+    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+    overflow: hidden;
+    border-radius: var(--borderRadius);
+    height: var(--heigth);
+    padding: var(--padding);
+`;
+
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  height: 100%;
+
+  /* Trim off corners when progress bar is near-full. */
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+    width: var(--width);
+    height: 100%;
+    background-color: ${COLORS.primary};
+    border-radius: 4px 0 0 4px;
+`;
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -4,15 +4,62 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
+// const SelectIcon = <Icon id="chevron-down" />;
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
+ 
 
   return (
-    <select value={value} onChange={onChange}>
+    <>
+    <SelectWrapper>
+    <SelectBox value={value} onChange={onChange}>
       {children}
-    </select>
+    </SelectBox>
+    <SelectIcon id="chevron-down" />
+    </SelectWrapper>
+    </>
+    
   );
 };
+
+const SelectWrapper = styled.div`
+  position: relative;
+  width: fit-content;
+`;
+
+const SelectBox = styled.select`
+
+  height: 48px;
+  background-color: ${COLORS.transparentGray15};
+  border: none;
+  border-radius: 8px;
+  padding: 12px 52px 12px 16px;
+  color: ${COLORS.gray700};
+  font-family: "Roboto", sans-serif;
+  font-size: 1rem;
+
+  /* Hiding arrow */
+  appearance: none;
+
+  &:focus {
+    border: ${COLORS.primary};
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const SelectIcon = styled(Icon)`
+  position: absolute; 
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  pointer-events: none;
+  width: 24px;
+  height: 24px;
+`;
 
 export default Select;
